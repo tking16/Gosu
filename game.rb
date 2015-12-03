dd38736bdbb886250a541abea92d6630c8c3039e
TKing <TKing@spartaglobal.com>
2015-12-03 15:37:50 +0000

    added text input

--- game.rb
+++ game.rb
@@ -24,13 +24,10 @@ class Player
       @direction = :left
       @x += -5 
       @x = @min_x if @x < @min_x
+
     end
     
-    def wall
-      if Gosu::distance(@x,@y,wall.x,wall.y) < 35
-        @x=0
-      end
-    end
+    
     
     if @window.button_down? Gosu::KbRight
     @direction = :right
@@ -55,30 +52,115 @@ end
 
 
 class Wall
+  attr_accessor :x, :y
   def initialize img, x, y
     @img = img
-    @x = @y = 0
+    @x = 500
+    @y = 440
   end
   
   def draw
-    @img.draw(500,440,1)
+    @img.draw(@x,@y,1)
   end
 end
 
-class Game < Gosu::Window
+class Input < Gosu::TextInput  
+  attr_reader :x, :y
+  INACTIVE_COLOR = 0xcc666666
+  ACTIVE_COLOR = 0xccff6666
+  SELECTION_COLOR = 0xcc0000ff
+  CARET_COLOR     = 0xffffffff
+  PADDING = 5
 	
+  def initialize window, font, x, y
+    super()
+    @window = window
+    @font = font
+    @x = x
+    @y = y
+    self.text = "Enter code here!"
+  end
+  
+  def draw
+    if @window.text_input == self
+      background_color = ACTIVE_COLOR
+    else
+      background_color = INACTIVE_COLOR
+    end
+    @window.draw_quad(x - PADDING,         y - PADDING,          background_color,
+                      x + width + PADDING, y - PADDING,          background_color,
+                      x - PADDING,         y + height + PADDING, background_color,
+                      x + width + PADDING, y + height + PADDING, background_color, 0)
+    pos_x = x + @font.text_width(self.text[0...self.caret_pos])
+    sel_x = x + @font.text_width(self.text[0...self.selection_start])
+    
+    
+    	    # Draw the selection background, if any; if not, sel_x and pos_x will be
+    # the same value, making this quad empty.
+    @window.draw_quad(sel_x, y,          SELECTION_COLOR,
+                      pos_x, y,          SELECTION_COLOR,
+                      sel_x, y + height, SELECTION_COLOR,
+                      pos_x, y + height, SELECTION_COLOR, 0)
+    
+	    if @window.text_input == self then
+      @window.draw_line(pos_x, y,          CARET_COLOR,
+                        pos_x, y + height, CARET_COLOR, 0)
+    end
+# draw the text!
+    @font.draw(self.text, x, y, 0)
+        
+end
+    
+    def width
+    @font.text_width(self.text)
+  end
+        
+  def height
+    @font.height
+  end
+        
+  def under_point?(mouse_x, mouse_y)
+    mouse_x > x - PADDING and mouse_x < x + width + PADDING and
+    mouse_y > y - PADDING and mouse_y < y + height + PADDING
+  end
+        
+# Tries to move the caret to the position specifies by mouse_x
+  def move_caret(mouse_x)
+    # Test character by character
+    1.upto(self.text.length) do |i|
+      if mouse_x < x + @font.text_width(text[0...i]) then
+        self.caret_pos = self.selection_start = i - 1;
+        return
+      end
+    end
+    # Default case: user must have clicked the right edge
+    self.caret_pos = self.selection_start = self.text.length
+  end
+        
+  end
+
+class Game < Gosu::Window
 	def initialize width=1000, height = 600, fullscreen=false
 		super
     @sprite = Player.new self
-    @wall = Wall.new Gosu::Image.new("media/tile.png"), 300, 430
+    @wall = Wall.new Gosu::Image.new("media/tile.png"), 300, 460
     @backdrop = Gosu::Image.new(self, "media/background1.png", false)
     @foreground = Gosu::Image.new(self, "media/foreground.png", false)
     @sprite.max_x = @foreground.width
     @x = @y = 0
+    font = Gosu::Font.new(self, Gosu::default_font_name, 20)
+    
+    @text_input = Input.new(self, font, 50, 30)
+    @cursor = Gosu::Image.new(self, "media/tile.png", false)
 	end
   
   def update
     @sprite.update
+    
+          
+    if @sprite.x > @wall.x - 100
+      @sprite.x= @wall.x - 100
+      end
   end
 	
   def draw
@@ -90,10 +172,21 @@ class Game < Gosu::Window
     @sprite.draw
       @wall.draw
     end
+    
+    @text_input.draw
+    @cursor.draw(mouse_x, mouse_y, 0)
   end
   
+  
   def button_down id
-    close if id == Gosu::KbEscape
+    if id == Gosu::KbEscape
+      close
+      elsif id == Gosu::MsLeft
+      self.text_input = @text_input
+      self.text_input.move_caret(mouse_x) unless self.text_input.nil?
+    elsif id == Gosu::KbEnter
+      self.text_input.x = -500
+    end
   end
   #paralax
   def plx_ratio
